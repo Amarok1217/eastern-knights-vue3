cb2b293a9adbe8b5efac7c25912edb187388611f
张宝冬 <baodong.zhang@argrace.ai>
2020-12-10 18:05:16 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <button
+      v-for="(item,index) in list"
+      :key="index"
+      @click="chooseItem(index)"
+    >{{index}}.{{item}}</button>
+    <div>{{currentItem}}</div>
   </div>
-  <router-view/>
 </template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+  name: 'App',
+  setup() {
+    const text: string = 'aaa';
+    const list = ref(['a', 'b', 'c']);
+    const currentItem = ref('');
+    const chooseItem = (index: number) => {
+      console.log(index);
+      currentItem.value = list.value[index];
+    };
+
+    return { list, currentItem, chooseItem };
+  },
+});
+</script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
 
-#nav {
-  padding: 30px;
+// #nav {
+//   padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
 </style>
